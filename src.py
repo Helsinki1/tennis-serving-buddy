aa67de0fbe921d75b6ef754f173355ce6a2bc55e
Helsinki1 <davidxionghl@gmail.com>
2024-06-10 11:12:41 -0400

    livestream capture, grayscale, edge detection, line detection

---
+++ src.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+camera = cv.VideoCapture(0);
+if not camera.isOpened():
+   print("ERROR: camera couldnt be opened")
+   exit()
+while True:
+   captured, frame = camera.read()
+   if not captured:
+      print("ERROR: frame captured incorrectly, exiting...")
+      break
+   grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+   t_lower = 50
+   t_upper = 150
+   L2Grad = True
+   edgesFrame = cv.Canny(grayFrame, t_lower, t_upper, L2gradient = L2Grad)
+
+   rho = 1 # distance resolution
+   theta = np.pi/180 # angular resolution
+   threshold = 15 # min number of votes
+   lines = cv.HoughLines(edgesFrame, rho, theta, threshold)
+
+   # draw lines
+   for line in lines:
+      for x1,y1,x2,y2 in line:
+         cv.line(frame, (x1,y1),(x2,y2),(255,0,0),5)
+
+   if cv.waitKey(1) == ord('q'):
+      break
+
+#conclude by exiting from everything
+camera.release
+cv.destroyAllWindows()
