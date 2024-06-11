fc110959575b4720016618f2a66024d70df0b3fc
Helsinki1 <davidxionghl@gmail.com>
2024-06-11 16:59:56 -0400

    added object detection

--- src.py
+++ src.py
@@ -2,15 +2,34 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-camera = cv.VideoCapture(0);
+camera = cv.VideoCapture(0)
 if not camera.isOpened():
    print("ERROR: camera couldnt be opened")
    exit()
+
+objectDetector = cv.createBackgroundSubtractorMOG2(history=100, varThreshold=30)
+
 while True:
+
    captured, frame = camera.read()
    if not captured:
       print("ERROR: frame captured incorrectly, exiting...")
       break
+
+   # detect objects
+   mask = objectDetector.apply(frame)
+   _, mask = cv.threshold(mask,254,255,cv.THRESH_BINARY)
+   contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+   
+   frameCopy1 = np.copy(frame) * 0
+   for cnt in contours:
+      # remove static & small elements
+      area = cv.contourArea(cnt)
+      if area > 20:
+         cv.drawContours(frameCopy1, [cnt], -1, (0,255,0), 2)
+   cv.imshow('objects', frameCopy1)
+
+   # detect lines
    grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
    t_lower = 50
@@ -26,12 +45,15 @@ while True:
    lines = cv.HoughLinesP(edgesFrame, rho, theta, threshold, np.array([]), minLineLength, maxLineGap)
 
    # draw lines
-   frameCopy = np.copy(frame) * 0
+   frameCopy2 = np.copy(frame) * 0
    for line in lines:
       for x1,y1,x2,y2 in line:
-         cv.line(frameCopy, (x1,y1),(x2,y2),(255,0,0),5)
-         frameWithEdges = cv.addWeighted(frame, 0.8, frameCopy, 1, 0)
-         cv.imshow('lines',frameWithEdges)
+         cv.line(frameCopy2, (x1,y1),(x2,y2),(255,0,0),5)
+         frameWithEdges = cv.addWeighted(frame, 0.8, frameCopy2, 1, 0)
+   cv.imshow('lines',frameWithEdges)
+
+
+
 
    if cv.waitKey(1) == ord('q'):
       break
