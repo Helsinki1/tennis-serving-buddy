dc5595074e7a58f47b0cc304d845809bf03b1d91
Helsinki1 <davidxionghl@gmail.com>
2024-07-01 10:23:47 -0400

    currently plotting way too many lines, pyplot is crashing

--- balltraj.py
+++ balltraj.py
@@ -1,7 +1,8 @@
+# src code as of 6/24/24, its able to plot ball traj on pyplot
+
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-
 camera = cv.VideoCapture(0)
 if not camera.isOpened():
    print("ERROR: camera couldnt be opened")
@@ -9,12 +10,9 @@ if not camera.isOpened():
 
 objectDetector = cv.createBackgroundSubtractorMOG2(history=100, varThreshold=30)
 
-#for pyplot
+#for matplotlib
 ballx = []
 bally = []
-coordPairs = [] # 2D array
-boxCaptured = False
-
 success, frame = camera.read()
 Fheight, Fwidth = frame.shape[0:2]
 
@@ -24,8 +22,7 @@ while True:
    if not captured:
       print("ERROR: frame captured incorrectly, exiting...")
       break
-
-   # detect objects & draw objects
+   # detect objects & draw objects (MIGHT BE REDUNDANT BC OBJ TRACKER)
    mask = objectDetector.apply(frame)
    _, mask = cv.threshold(mask,254,255,cv.THRESH_BINARY)
    contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
@@ -37,37 +34,27 @@ while True:
       area = cv.contourArea(cnt)
       if area > 35:
          x, y, w, h = cv.boundingRect(cnt)
-         cv.rectangle(blankFrame, (x, y), (x+w, y+h), (0, 0, 200), 3) # SUPPRESS IN FINAL PRODUCT
          ballx.append( Fwidth - (x + (w//2)) )
          bally.append( Fheight - (y + h) )
 
 
    # detect lines
    grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
    t_lower = 50
    t_upper = 150
    L2Grad = True
    edgesFrame = cv.Canny(grayFrame, t_lower, t_upper, L2gradient = L2Grad)
-
    rho = 1 # distance resolution
    theta = np.pi/180 # angular resolution
    threshold = 15 # min number of votes
    minLineLength = 50 # min pixels to be a line
    maxLineGap = 30 # max pixels between parts of a line
    lines = cv.HoughLinesP(edgesFrame, rho, theta, threshold, np.array([]), minLineLength, maxLineGap)
-
-   numLines = len(lines)
-   coordPairs = [[0,0,0,0] for i in range(numLines)]
-   ind = 0
    # draw lines
    for line in lines:
       for x1,y1,x2,y2 in line:
-         cv.line(blankFrame, (x1,y1),(x2,y2),(255,0,0),5) # SUPPRESS IN FINAL PRODUCT
-         coordPairs[ind] = [x1,y1,x2,y2]
-         ind += 1
-
-   cv.imshow('Lines & Objects', blankFrame) # SUPPRESS IN FINAL PRODUCT
+         cv.line(blankFrame, (x1,y1),(x2,y2),(255,0,0),5)
+   cv.imshow('Lines & Objects',blankFrame)
 
 
    if cv.waitKey(1) == ord('q'):
@@ -76,17 +63,11 @@ while True:
 
 #graph ball trajectory
 plt.plot(ballx, bally, 'ro')
+plt.axline((0,0), (20,20), linewidth=4, color='b')
+plt.axline((0,0), (20,35), linewidth=4, color='b')
+plt.axline((0,0), (20,50), linewidth=4, color='b')
 plt.axis((0,Fwidth,0,Fheight))
-
-#graph edges of serve box
-for [x1,y1,x2,y2] in coordPairs:
-   print(x1, y1, x2, y2) # PRINT OUTPUT FOR DEBUGGING
-   plt.axline((x1,y1),(x2,y2), linewidth=2, color='b')
-plt.show
-
-#find lowest point of ball traj (the bounce)
-index = bally.index(min(bally))
-bounce = [ballx[index], bally[index]]
+plt.show()
 
 #conclude by exiting from everything
 camera.release
