3d952656621574d0aa480426680da4afca3fee0a
Helsinki1 <davidxionghl@gmail.com>
2024-07-02 13:06:50 -0400

    saved nonthreading version, src is now an untested threading model

---
+++ nonThreadedVersion.py
@@ -0,0 +1,104 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+camera = cv.VideoCapture(0)
+if not camera.isOpened():
+   print("ERROR: camera couldnt be opened")
+   exit()
+
+objectDetector = cv.createBackgroundSubtractorMOG2(history=100, varThreshold=30)
+
+#for pyplot
+ballx = []
+bally = []
+coordPairs = np.array([[]], ndmin=2)
+
+success, frame = camera.read()
+Fheight, Fwidth = frame.shape[0:2]
+
+ans = input("Do you want to select ROI? (y/n) ")
+if ans == "y":
+   croppedx, croppedy, croppedw, croppedh = cv.selectROI("select ROI", frame) # allow user to manually exclude background
+   Fheight = croppedh
+   Fwidth = croppedw
+
+while True:
+
+   captured, frame = camera.read()
+   if not captured:
+      print("ERROR: frame captured incorrectly, exiting...")
+      break
+   if ans == "y":
+      frame = frame[croppedx:(croppedx+croppedw+1), croppedy:(croppedy+croppedh+1)]
+
+   # detect objects & draw objects
+   mask = objectDetector.apply(frame)
+   _, mask = cv.threshold(mask,254,255,cv.THRESH_BINARY)
+   contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+   
+   blankFrame = np.copy(frame) * 0
+   for cnt in contours:
+      # remove static & small elements
+      # draw boxes around all the detected objects & record ball coords
+      area = cv.contourArea(cnt)
+      if area > 35:
+         x, y, w, h = cv.boundingRect(cnt)
+         cv.rectangle(blankFrame, (x, y), (x+w, y+h), (0, 0, 200), 3) # SUPPRESS IN FINAL PRODUCT
+         ballx.append( Fwidth - (x + (w//2)) )
+         bally.append( Fheight - (y + h) )
+
+   # detect lines
+   grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+   t_lower = 50
+   t_upper = 150
+   L2Grad = True
+   edgesFrame = cv.Canny(grayFrame, t_lower, t_upper, L2gradient = L2Grad)
+
+   rho = 1 # distance resolution
+   theta = np.pi/180 # angular resolution
+   threshold = 15 # min number of votes
+   minLineLength = 50 # min pixels to be a line
+   maxLineGap = 30 # max pixels between parts of a line
+   lines = cv.HoughLinesP(edgesFrame, rho, theta, threshold, np.array([]), minLineLength, maxLineGap)
+
+   # draw lines
+   for line in lines:
+      for x1,y1,x2,y2 in line:
+         cv.line(blankFrame, (x1,y1),(x2,y2),(255,0,0),5) # SUPPRESS IN FINAL PRODUCT
+
+   cv.imshow('Lines & Objects', blankFrame) # SUPPRESS IN FINAL PRODUCT
+
+
+   # take a snapshot of the court lines on the last captured frame
+   if cv.waitKey(1) == ord('q'):
+      numLines = len(lines)
+      coordPairs = [[0,0,0,0] for i in range(numLines)]
+      ind = 0
+      for line in lines:
+         for x1,y1,x2,y2 in line:
+            coordPairs[ind] = [x1,y1,x2,y2]
+            ind += 1
+      break
+
+
+
+#graph edges of serve box
+for [x1,y1,x2,y2] in coordPairs:
+   plt.axline((x1,y1),(x2,y2), linewidth=2, color='b')
+
+#graph ball trajectory
+plt.plot(ballx, bally, 'ro')
+plt.axis((0,Fwidth,0,Fheight))
+
+#plot lowest point of ball traj (the bounce)
+index = bally.index(min(bally))
+bounce = [ballx[index], bally[index]]
+plt.plot(bounce[0], bounce[1], 'go')
+plt.show()
+
+#conclude by exiting from everything
+camera.release
+cv.destroyAllWindows()
+
--- src.py
+++ src.py
@@ -1,3 +1,4 @@
+import threading
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
@@ -23,81 +24,101 @@ if ans == "y":
    Fheight = croppedh
    Fwidth = croppedw
 
-while True:
 
-   captured, frame = camera.read()
-   if not captured:
-      print("ERROR: frame captured incorrectly, exiting...")
-      break
-   if ans == "y":
-      frame = frame[croppedx:(croppedx+croppedw+1), croppedy:(croppedy+croppedh+1)]
+def updateCamera():
+   while True:
+      captured, img = camera.read()
+      if not captured:
+         print("ERROR: frame captured incorrectly, exiting...")
+         break
 
-   # detect objects & draw objects
-   mask = objectDetector.apply(frame)
-   _, mask = cv.threshold(mask,254,255,cv.THRESH_BINARY)
-   contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-   
-   blankFrame = np.copy(frame) * 0
-   for cnt in contours:
-      # remove static & small elements
-      # draw boxes around all the detected objects & record ball coords
-      area = cv.contourArea(cnt)
-      if area > 35:
-         x, y, w, h = cv.boundingRect(cnt)
-         cv.rectangle(blankFrame, (x, y), (x+w, y+h), (0, 0, 200), 3) # SUPPRESS IN FINAL PRODUCT
-         ballx.append( Fwidth - (x + (w//2)) )
-         bally.append( Fheight - (y + h) )
+      global frame
+      if ans == "y":
+         frame = img[croppedx:(croppedx+croppedw+1), croppedy:(croppedy+croppedh+1)]
+      else:
+         frame = img
 
-   # detect lines
-   grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-   t_lower = 50
-   t_upper = 150
-   L2Grad = True
-   edgesFrame = cv.Canny(grayFrame, t_lower, t_upper, L2gradient = L2Grad)
-
-   rho = 1 # distance resolution
-   theta = np.pi/180 # angular resolution
-   threshold = 15 # min number of votes
-   minLineLength = 50 # min pixels to be a line
-   maxLineGap = 30 # max pixels between parts of a line
-   lines = cv.HoughLinesP(edgesFrame, rho, theta, threshold, np.array([]), minLineLength, maxLineGap)
-
-   # draw lines
-   for line in lines:
-      for x1,y1,x2,y2 in line:
-         cv.line(blankFrame, (x1,y1),(x2,y2),(255,0,0),5) # SUPPRESS IN FINAL PRODUCT
-
-   cv.imshow('Lines & Objects', blankFrame) # SUPPRESS IN FINAL PRODUCT
+      if cv.waitKey(1) == ord('q'):
+         break
 
 
-   # take a snapshot of the court lines on the last captured frame
-   if cv.waitKey(1) == ord('q'):
-      numLines = len(lines)
-      coordPairs = [[0,0,0,0] for i in range(numLines)]
-      ind = 0
+def processFrame():
+   while True:
+      # detect objects & draw objects
+      global frame
+      mask = objectDetector.apply(frame)
+      _, mask = cv.threshold(mask,254,255,cv.THRESH_BINARY)
+      contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+      blankFrame = np.copy(frame) * 0
+      for cnt in contours:
+         # remove static & small elements
+         # draw boxes around all the detected objects & record ball coords
+         area = cv.contourArea(cnt)
+         if area > 35:
+            x, y, w, h = cv.boundingRect(cnt)
+            cv.rectangle(blankFrame, (x, y), (x+w, y+h), (0, 0, 200), 3) # SUPPRESS IN FINAL PRODUCT
+            ballx.append( Fwidth - (x + (w//2)) )
+            bally.append( Fheight - (y + h) )
+
+      # detect lines
+      grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+      t_lower = 50
+      t_upper = 150
+      L2Grad = True
+      edgesFrame = cv.Canny(grayFrame, t_lower, t_upper, L2gradient = L2Grad)
+
+      rho = 1 # distance resolution
+      theta = np.pi/180 # angular resolution
+      threshold = 15 # min number of votes
+      minLineLength = 50 # min pixels to be a line
+      maxLineGap = 30 # max pixels between parts of a line
+      lines = cv.HoughLinesP(edgesFrame, rho, theta, threshold, np.array([]), minLineLength, maxLineGap)
+
+      # draw lines
       for line in lines:
          for x1,y1,x2,y2 in line:
-            coordPairs[ind] = [x1,y1,x2,y2]
-            ind += 1
-      break
+            cv.line(blankFrame, (x1,y1),(x2,y2),(255,0,0),5) # SUPPRESS IN FINAL PRODUCT
+
+      cv.imshow('Lines & Objects', blankFrame) # SUPPRESS IN FINAL PRODUCT
+
+      # take a snapshot of the court lines on the last captured frame
+      if cv.waitKey(1) == ord('q'):
+         numLines = len(lines)
+         coordPairs = [[0,0,0,0] for i in range(numLines)]
+         ind = 0
+         for line in lines:
+            for x1,y1,x2,y2 in line:
+               coordPairs[ind] = [x1,y1,x2,y2]
+               ind += 1
+         break
 
 
+if __name__ == "__main__":
+   t1 = threading.Thread(target=updateCamera, args=())
+   t2 = threading.Thread(target=processFrame, args=())
 
-#graph edges of serve box
-for [x1,y1,x2,y2] in coordPairs:
-   plt.axline((x1,y1),(x2,y2), linewidth=2, color='b')
+   t1.start()
+   t2.start()
 
-#graph ball trajectory
-plt.plot(ballx, bally, 'ro')
-plt.axis((0,Fwidth,0,Fheight))
+   t1.join()
+   t2.join()
 
-#plot lowest point of ball traj (the bounce)
-index = bally.index(min(bally))
-bounce = [ballx[index], bally[index]]
-plt.plot(bounce[0], bounce[1], 'go')
-plt.show()
+   #graph edges of serve box
+   for [x1,y1,x2,y2] in coordPairs:
+      plt.axline((x1,y1),(x2,y2), linewidth=2, color='b')
 
-#conclude by exiting from everything
-camera.release
-cv.destroyAllWindows()
+   #graph ball trajectory
+   plt.plot(ballx, bally, 'ro')
+   plt.axis((0,Fwidth,0,Fheight))
+
+   #plot lowest point of ball traj (the bounce)
+   index = bally.index(min(bally))
+   bounce = [ballx[index], bally[index]]
+   plt.plot(bounce[0], bounce[1], 'go')
+   plt.show()
+
+   #conclude by exiting from everything
+   camera.release
+   cv.destroyAllWindows()
