ef43dee4872c0221a44e12fd23b52d81b3132942
Helsinki1 <davidxionghl@gmail.com>
2024-07-25 18:40:43 -0400

    successfully sorted critical vertices by y-value, and added blue polygon patch for court area

--- src.py
+++ src.py
@@ -62,7 +62,7 @@ while True:
       L2Grad = True
       edgesFrame = cv.Canny(grayFrame, t_lower, t_upper, L2gradient = L2Grad)
 
-      kernel = np.ones((7,7), np.uint8) # creates 3x3 array full of 1's in uint8
+      kernel = np.ones((5,5), np.uint8) # creates 3x3 array full of 1's in uint8
       edgesFrame = cv.dilate(edgesFrame, kernel, iterations=1) # enlarges white lines to merge both borders of a line into one edge
       kernel = np.ones((7,7), np.uint8)
       edgesFrame = cv.erode(edgesFrame, kernel, iterations=1) # thins white lines to merge edges so houghlines doesnt mark down too many lines
@@ -95,25 +95,6 @@ fig, ax = plt.subplots()
 ax.set_facecolor("yellowgreen")
 
 
-# take a snapshot of the court lines on the last captured frame
-numLines = len(lines)
-coordPairs = [[0,0,0,0] for i in range(numLines)]
-ind = 0
-for line in lines:
-   for x1,y1,x2,y2 in line:
-      coordPairs[ind] = [x1,y1,x2,y2]
-      ind += 1
-
-#graph all detected court lines
-for [x1,y1,x2,y2] in coordPairs:
-   ax.plot([x1,x2],[y1,y2], color='white', linestyle='solid', linewidth=2)
-   plt.plot(x1,y1, color='m')
-   plt.plot(x2,y2, color='m')
-   #plt.axline((x1,y1),(x2,y2), linewidth=2, color='w')
-   #plt.plot([x1,x2],[y1,y2], 'w', linestyle="solid")
-
-
-
 # record all the corners created by court lines
 grayFrame = np.float32(grayFrame)
 dst = cv.cornerHarris(grayFrame, 2, 3, 0.04)  # find harris corners
@@ -131,19 +112,53 @@ cv.imshow("grayscale + dilate + threshold for corners", dst)
 
 # plot all detected corners
 corners = np.intp(corners)
-print(corners)
-cornersX = []
-cornersY = []
 for [x,y] in corners[1:]:
-   cornersX.append(x)
-   cornersY.append(y)
    plt.plot(x, y, 'mo')
 
 
 #draw the tennis court using patches
-         #serveVertices = np.array((1,1),(1,2),(2,1),(2,2)) # PLACEHOLDER CONSTANTS
-         #shape = patches.Polygon(serveVertices, color="cornflowerblue")
-         #ax.add_patch(shape)
+vertices = []
+for [x,y] in corners[1:]:
+   vertices.append([x,y]) # switch from numpy array to python array
+
+def sortFirst(arr):
+   return arr[0]
+def sortSecond(arr):
+   return arr[1]
+vertices.sort(reverse=True, key=sortSecond) # baseline vertices first, then service, then net
+baseLine = vertices[0:4]
+baseLine.sort(key=sortFirst) # the four vertices of the baseline, L to R
+serviceLine = vertices[4:7]
+serviceLine.sort(key=sortFirst) # the three vertices of serviceline, L to R
+netLine = vertices[7:]
+netLine.sort(key=sortFirst) # the seven vertices of the netline, L to R
+
+courtTL = (baseLine[0][0], baseLine[0][1])
+courtTR = (baseLine[3][0], baseLine[3][1])
+courtBR = (netLine[5][0], netLine[5][1])
+courtBL = (netLine[1][0], netLine[1][1])
+serveVertices = np.array([courtTL, courtTR, courtBR, courtBL]) # PLACEHOLDER CONSTANTS
+shape = patches.Polygon(serveVertices, color="cornflowerblue")
+ax.add_patch(shape)
+
+
+
+# take a snapshot of the court lines on the last captured frame
+numLines = len(lines)
+coordPairs = [[0,0,0,0] for i in range(numLines)]
+ind = 0
+for line in lines:
+   for x1,y1,x2,y2 in line:
+      coordPairs[ind] = [x1,y1,x2,y2]
+      ind += 1
+
+#graph all detected court lines
+for [x1,y1,x2,y2] in coordPairs:
+   ax.plot([x1,x2],[y1,y2], color='white', linestyle='solid', linewidth=2)
+   plt.plot(x1,y1, color='m')
+   plt.plot(x2,y2, color='m')
+   #plt.axline((x1,y1),(x2,y2), linewidth=2, color='w')
+   #plt.plot([x1,x2],[y1,y2], 'w', linestyle="solid")
 
 #graph ball trajectory
 plt.plot(ballx, bally, 'ro')
